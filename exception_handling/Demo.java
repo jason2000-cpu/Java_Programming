121f7cc6aec523e14d27b9b2a06d9103e56c0ed3
Mwangi-Jack <jasonmuturi2000@gmail.com>
2025-06-30 18:29:44 +0300

    creating a custom exception

--- exception_handling/Demo.java
+++ exception_handling/Demo.java
@@ -1,4 +1,6 @@
+
 public class Demo {
+
     public static void main(String a[]) {
         int i = 2;
         int j = 18;
@@ -8,7 +10,7 @@ public class Demo {
         int nums[] = new int[5];
 
         try {
-            result = j/i;
+            result = j / i;
             System.err.println(str.length());
             System.out.println(nums[1]);
             System.out.println(nums[5]);
@@ -20,7 +22,7 @@ public class Demo {
         } catch (Exception e) { //The general exception catch should always be last
             System.out.println("An Unknown Error Occured");
         }
-        
+
         System.out.println("The Result Is :::: " + result);
     }
 }
